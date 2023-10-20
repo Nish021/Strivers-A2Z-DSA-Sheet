e9a9a3b35947b1cf861b4f0022037b858134b25f
Nish021 <69570720+Nish021@users.noreply.github.com>
2023-10-20 23:25:20 +0530

    Create Print Divisor.java

---
+++ Basic Maths/Print Divisor.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static void print_divisors(int n) {
+        // code here
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= Math.sqrt(n); i++){
+            
+            if(n%i == 0){
+            list.add(i);
+    
+			if(i != n/i) 
+			list.add(n/i);
+            }
+        }
+         Collections.sort(list);
+        for(int i = 0; i <list.size(); i++){
+            System.out.print(list.get(i)+ " ");
+        }
+    }
+}
