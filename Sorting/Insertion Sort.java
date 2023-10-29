8b769f3ba4e51ec9f6874de47e3b05af461b20c8
Nish021 <69570720+Nish021@users.noreply.github.com>
2023-10-29 22:10:21 +0530

    Create Insertion Sort.java

---
+++ Sorting/Insertion Sort.java
@@ -0,0 +1,16 @@
+class Solution
+{
+  //Function to sort the array using insertion sort algorithm.
+  public void insertionSort(int arr[], int n)
+  { 
+      for(int i = 0; i < n; i++){
+          int j = i;
+          while(j > 0 && arr[j] < arr[j-1]){
+                    int t = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = t;
+                    j--;
+          }
+        }
+  }
+}
