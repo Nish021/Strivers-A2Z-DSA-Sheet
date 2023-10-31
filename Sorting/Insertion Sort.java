8209ec0f5d7c3d2e20cefc32119a23fffd1a05fc
Nish021 <69570720+Nish021@users.noreply.github.com>
2023-10-31 22:12:20 +0530

    Update Insertion Sort.java

--- Sorting/Insertion Sort.java
+++ Sorting/Insertion Sort.java
@@ -14,3 +14,25 @@ class Solution
         }
   }
 }
+
+//Recursive solution
+public class Solution {
+
+    static void Recursive(int i, int n, int[] arr){
+        if(i == n)
+        return;
+
+       int j = i;
+          while(j > 0 && arr[j] < arr[j-1]){
+                    int t = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = t;
+                    j--;
+          }
+
+        Recursive(i+1, n, arr);
+    }
+    public static void insertionSort(int[] arr, int size) {
+        Recursive(0, size, arr);
+    }
+}
